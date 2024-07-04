d47d7c36f430bbff49912a6dfa5211e5e7cd1323
Sarthak Chaure <sarchaure911@gmail.com>
2024-07-05 05:15:09 +0530

    fin

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask,render_template,Response
+import cv2 as cv 
+from main import serve
+
+app=Flask(__name__)
+camera=cv.VideoCapture(0)
+camera.set(3,1280) #width
+camera.set(4,720) #height
+history=[]
+
+def generate_frames():
+    while True:
+       
+        success,frame=camera.read()
+        if not success:
+            break
+        else:
+            try:
+                frame,finEq=serve(frame)
+            except:
+                pass
+            if finEq!="":
+                global history
+                history.append(finEq)
+                print(history)
+            ret,buffer=cv.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+            yield(b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        
+
+@app.route('/')
+def index():
+    # return "Starting"
+    return render_template('index.html')
+
+@app.route('/video')
+def video_function():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -1,143 +1,88 @@
 import cv2 as cv
 from cvzone.HandTrackingModule import HandDetector
-#import time
 
-class button:
-    def __init__(self,pos,width,height,value):
+class Button:
+    def __init__(self, pos, width, height, value):
+        self.pos = pos
+        self.width = width
+        self.height = height
+        self.value = value
 
-        self.pos=pos
-        self.width=width
-        self.height=height
-        self.value=value
-
-    def draw(self,frame):
-
-        cv.rectangle(frame, self.pos, (self.pos[0]+self.width,self.pos[1]+self.height),
+    def draw(self, frame):
+        cv.rectangle(frame, self.pos, (self.pos[0] + self.width, self.pos[1] + self.height),
                      (225, 255, 225), cv.FILLED)
         cv.rectangle(frame, self.pos, (self.pos[0] + self.width, self.pos[1] + self.height),
-                     (50, 50, 225), 4  )
-        #cv.rectangle(frame, (100, 100), (300, 300), (50, 50, 225), 4)
-
-        cv.putText(frame, self.value, (self.pos[0]+20 , self.pos[1]+50),
+                     (50, 50, 225), 4)
+        cv.putText(frame, self.value, (self.pos[0] + 20, self.pos[1] + 50),
                    cv.FONT_HERSHEY_PLAIN, 2, (50, 50, 225), 2)
-    def clickCheck(self,x,y):
 
-        #x1 < x < x1+width
-        if self.pos[0] < x < self.pos[0] + self.width:
-            if self.pos[1] < y < self.pos[1] + self.height:
+    def clickCheck(self, x, y, frame):
+        if self.pos[0] < x < self.pos[0] + self.width and self.pos[1] < y < self.pos[1] + self.height:
+            cv.rectangle(frame, self.pos, (self.pos[0] + self.width, self.pos[1] + self.height),
+                         (255, 255, 225), cv.FILLED)
+            cv.rectangle(frame, self.pos, (self.pos[0] + self.width, self.pos[1] + self.height),
+                         (0, 0, 50), 4)
+            cv.putText(frame, self.value, (self.pos[0] + 20, self.pos[1] + 50),
+                       cv.FONT_HERSHEY_PLAIN, 2, (50, 50, 25), 6)
+            return True
+        return False
 
-                cv.rectangle(frame, self.pos, (self.pos[0] + self.width, self.pos[1] + self.height),
-                             (255, 255, 225), cv.FILLED)
-                cv.rectangle(frame, self.pos, (self.pos[0] + self.width, self.pos[1] + self.height),
-                             (0, 0, 50), 4)
-                # cv.rectangle(frame, (100, 100), (300, 300), (50, 50, 225), 4)
+detect = HandDetector(detectionCon=0.9, maxHands=1)
 
-                cv.putText(frame, self.value, (self.pos[0] + 20, self.pos[1] + 50),
-                           cv.FONT_HERSHEY_PLAIN, 2, (50, 50, 25), 6)
-                return True
-            else:
-                return False
+butListVal = [
+    ['1', '2', '3', '+'],
+    ['4', '5', '6', '-'],
+    ['7', '8', '9', '*'],
+    ['0', '/', '.', '=']
+]
 
+butList = [Button((x * 100 + 800, y * 100 + 150), 100, 100, butListVal[y][x]) for x in range(4) for y in range(4)]
 
-
-
-
-# Webcam
-cap = cv.VideoCapture(0)
-
-cap.set(3,1280) #width
-cap.set(4,720) #height
-
-detect=HandDetector(detectionCon=0.9,maxHands=1)
-
-#Creating Buttons
-butListVal=[ ['1','2','3','+'],
-             ['4','5','6','-'],
-             ['7','8','9','*'],
-             ['0','/','.','=']]
-
-butList=[]
-for x in range(4):
-    for y in range(4):
-
-        xpos = x*100 +800
-        ypos = y*100 +150
-        butList.append(button((xpos,ypos),100,100,butListVal[y][x]))
-
-#Variable
-myEq= ""
-delayCounter =0
-finEq=" "
-while(True):
-
-    isTrue , frame = cap.read()
-
-    #flipping image
-    #0 vertically 1 horizontally
-    frame=cv.flip(frame,1)
-
-    #Detection of hand
-    hand , frame = detect.findHands(frame, flipType=False)
-
-    #Draw all buttons
-
-    cv.rectangle(frame, (800,50), (800 + 400, 70 + 100),
-                 (225, 255, 225), cv.FILLED)
-    cv.rectangle(frame, (800,50), (800 + 400, 70 + 100),
-                 (50, 50, 225), 4)
+myEq = ""
+delayCounter = 0
+finEq = ""
 
 
+def serve(frame):
+    global myEq, delayCounter, finEq
+    frame = cv.flip(frame, 1)
+
+    hand, frame = detect.findHands(frame, flipType=False)
+
+    cv.rectangle(frame, (800, 50), (1200, 150), (225, 255, 225), cv.FILLED)
+    cv.rectangle(frame, (800, 50), (1200, 150), (50, 50, 225), 4)
     for button in butList:
         button.draw(frame)
 
-    # Checking Hand
-    if hand:
-        lmList=hand[0]['lmList']
-        #lank mark list , has all the points of out fingers
 
-        length,info ,frame=detect.findDistance(lmList[8][:2],lmList[12][:2],frame)
-        # 8 is index and 12 is middle
-        print(length)
+    ev=""
+
+    if hand:
+        lmList = hand[0]['lmList']
+        length, info, frame = detect.findDistance(lmList[8][:2], lmList[12][:2], frame)
         x, y = lmList[8][:2]
 
-        if length<35:
-            for i,button in enumerate(butList):
-                if button.clickCheck(x,y) and delayCounter ==0:
-
-                    myVal=(butListVal[int(i%4)][int(i/4)])
-                    if(myVal=='='):
-
-                        finEq =str(eval(myEq))
-                        myEq=" "
-                    else:
+        if length < 35:
+            for i, button in enumerate(butList):
+                if button.clickCheck(x, y, frame) and delayCounter == 0:
+                    myVal = butListVal[int(i % 4)][int(i / 4)]
+                    if myVal == '=' and myEq != "":
+                        finEq = str(eval(myEq))
+                        ev=myEq+"="+finEq
+                        myEq = ""
+                    elif myVal != '=':
                         myEq += myVal
-                        finEq=" "
+                        finEq = ""
                     delayCounter = 1
-                    #time.sleep(0.2)
 
-
-
-    #Avoid Duplicates
     if delayCounter != 0:
-        delayCounter +=1
-        if delayCounter>10:
-            delayCounter=0
+        delayCounter += 1
+        if delayCounter > 10:
+            delayCounter = 0
 
-    # Draw the answerd
-    if(myEq==" "):
-        cv.putText(frame, finEq, (820, 114 ),
-                cv.FONT_HERSHEY_PLAIN, 3, (50, 50, 225), 3)
+    display_text = finEq if myEq == "" else myEq
+    cv.putText(frame, display_text, (820, 114), cv.FONT_HERSHEY_PLAIN, 3, (50, 50, 225), 3)
+    if ev!="":
+        return frame,ev
     else:
-        cv.putText(frame, myEq, (820, 114),
-                   cv.FONT_HERSHEY_PLAIN, 3, (50, 50, 225), 3)
-
-
-    #Displaying
-    #frame = cv.resize(frame, (900,800), interpolation=cv.INTER_AREA)
-
-    cv.imshow("Live Image",frame)
-    if cv.waitKey(20) & 0xFF == ord('d'):
-        break
-cap.release()
-cv.destroyAllWindows
-
+        return frame,""
